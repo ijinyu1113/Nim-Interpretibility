69b9ffd0b614f6c30107c11306bb8224f29ac887
Ijin Yu <ijinyu1114@gmail.com>
2026-04-01 16:32:34 -0700

    fix bugs

---
+++ run_finetune.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=finetune
+#SBATCH --output=logs/%x_%j.out
+#SBATCH --error=logs/%x_%j.err
+#SBATCH --partition=ghx4
+#SBATCH --account=benv-dtai-gh
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=64G
+#SBATCH --time=48:00:00
+export OMP_NUM_THREADS=8
+export OPENBLAS_NUM_THREADS=8
+export MKL_NUM_THREADS=8
+export NUMEXPR_NUM_THREADS=8
+export VECLIB_MAXIMUM_THREADS=8
+export TOKENIZERS_PARALLELISM=false
+
+
+module reset
+module load python/miniforge3_pytorch/2.7.0
+source $(conda info --base)/etc/profile.d/conda.sh
+conda activate nim-env
+cd /u/iyu1/nim_game_project/access_files
+
+# $1 = max_remove (e.g. 7), $2 = seed (e.g. 42), $3 = lr (e.g. 3e-5)
+timeout 48h python -u finetune_nim_evalattempt.py $1 $2 $3
